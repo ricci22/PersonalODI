7b3487ed2622e1550ad986929f9c65e3273ab32c
Nanda Rizki Nasution <nandarizki.nasution@gmail.com>
2023-11-26 18:53:05 +0700

    20231126 Checkpoint GOAPI insert SQL

--- Script/GOAPI_Companies.sql
+++ Script/GOAPI_Companies.sql
@@ -1,3 +1,5 @@
+-- -- select * from HCM_NN_JSON;
+
 DECLARE
   l_clob    clob;
   l_buffer         varchar2(32767);
@@ -9,14 +11,17 @@ BEGIN
         p_value_01       => 'application/json',
         p_name_02        => 'X-API-KEY',
         p_value_02       => '312690fb-0d36-5231-9c6b-07c9af4a'
-
         );
 
     DBMS_OUTPUT.put_line('l_clob=');
 
-    l_clob := apex_web_service.make_rest_request(        
-        p_url => 'https://api.goapi.io/stock/idx/companies',
-        p_http_method => 'GET'
+
+    insert into HCM_NN_JSON( 
+        document 
+    ) values (
+        apex_web_service.make_rest_request(
+            p_url         => 'https://api.goapi.io/stock/idx/companies',
+            p_http_method => 'GET' )
     );
 
     DBMS_OUTPUT.put_line('l_clob=');
@@ -25,7 +30,7 @@ BEGIN
     l_offset := 1;
     BEGIN
         DBMS_OUTPUT.put_line(TO_CHAR(apex_web_service.g_status_code));
-        DBMS_OUTPUT.put_line(l_clob);
+        -- DBMS_OUTPUT.put_line(l_clob);
     EXCEPTION
         WHEN no_data_found THEN
             NULL;
@@ -33,3 +38,4 @@ BEGIN
 
 END;
 
+-- select id, fetched_at, dbms_lob.getlength(document) from HCM_NN_JSON;
