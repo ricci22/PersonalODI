3cbc43b3045268939f70779a482f56fb3e8f19f9
Nanda Rizki Nasution <nandarizki.nasution@gmail.com>
2023-11-29 12:30:08 +0700

    20231129 Checkpoint PD

---
+++ Script/APEX_InsertIntoFromCLOB.sql
@@ -0,0 +1,35 @@
+insert into HCM_NN_SYMBOL (
+    symbol
+    ,about
+    ,sector_name
+    ,sub_industry_name
+    ,sub_sector_name
+    ,website
+    ,address
+    ,name
+)
+select 
+    e.symbol
+    ,e.about
+    ,e.sector_name
+    ,e.sub_industry_name
+    ,e.sub_sector_name
+    ,e.website
+    ,e.address
+    ,e.name           
+    from HCM_NN_JSON j, json_table(
+        document,
+        '$.data[*]'
+        columns(
+            symbol    varchar2(10)  path '$.symbol',
+            about    varchar2(254)  path '$.about',
+            sector_name    varchar2(50)  path '$.sector_name',
+            sub_industry_name    varchar2(50)  path '$.sub_industry_name',
+            sub_sector_name    varchar2(254)  path '$.symbol',
+            website    varchar2(254)  path '$.website',
+            address    varchar2(254)  path '$.address',
+            name      varchar2(50)  path '$.name' ) ) e
+    where j.name = 'profile'
+        and j.id = (
+            select max(id) from HCM_NN_JSON where name = j.name)
+
